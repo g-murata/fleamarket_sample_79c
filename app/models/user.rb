8660b0bfae3b40f3c459fd18f771ae125ab95eb7
furuseki8888 <dassentv0202@gmail.com>
2020-07-26 19:30:57 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, password_length: 7..128
-  validates :nickname, :last_name, :first_name, :birthday, presence: true
-  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ぁ-んー－]+\z/}
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[ぁ-んー－]+\z/ }
+
+  has_one :user_address, dependent: :destroy
 end
