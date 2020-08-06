1b0876e599cc2c00f92b19a880b92b579d3d89ff
kentarohosobuchi <kentaro.khoso@gmail.com>
2020-08-06 19:52:59 +0900

    ancestry_edit_page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -49,6 +49,24 @@ class ProductsController < ApplicationController
     @category_parent_array = Category.where(ancestry: nil)
     # binding.pry
     # @products = ProductImage.where(product_id:params[:id])              #投稿に紐づく画像を取得する
+    grandchild_category = @product.category
+    child_category = grandchild_category.parent
+
+    @category_parent_array = []
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+
+    @category_children_array = []
+    Category.where(ancestry: child_category.ancestry).each do |children|
+      @category_children_array << children
+    end
+
+    @category_grandchildren_array = []
+    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
+      @category_grandchildren_array << grandchildren
+    end
+  
   end
 
   def update
