fed8240ef9e5c357fa9300b159afe5f45b23de7a
kentarohosobuchi <kentaro.khoso@gmail.com>
2020-08-06 17:38:44 +0900

    Show_changed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,10 +32,9 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @category_id = @product.category_id
-    @category_parent = Category.find(@category_id).parent.parent
-    @category_child = Category.find(@category_id).parent
-    @category_grandchild = Category.find(@category_id)
+    @category_grandchild = @product.category
+    @category_child = @category_grandchild.parent
+    @category_parent = @category_child.parent
   end
 
   def get_category_children
