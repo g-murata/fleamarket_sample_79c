833e97eb4bad27178521b5eb2cddae8ae7073547
TomozQ <inf.4clover@gmail.com>
2020-08-11 19:38:07 +0900

    facebookログインのリンクをコメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_06_095351) do
-
-# ActiveRecord::Schema.define(version: 2020_08_04_152723) do
-
-
-# ActiveRecord::Schema.define(version: 2020_08_05_093222) do
-
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
